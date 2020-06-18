cf32c7d21bc03897854f160679ff8af4e6fa53e7
Grigory <thegriglat@gmail.com>
2020-06-18 23:24:11 +0300

    Update test

--- test.cpp
+++ test.cpp
@@ -14,18 +14,31 @@ struct Item
     int a;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     int c = 5;
-    if (argc > 1){
+    if (argc > 1)
+    {
         c = atoi(argv[1]);
     }
-    BinaryFile<Header, Item> bf("binary.bin");
+    BinaryFile<Header, Item> bf("binary.bin", 9);
     bf.writeHeader({.version = 2});
-    for (int i = 0; i < c; ++i)
+    cout << "### WRITE ###" << endl;
+    for (int i = 1200; i < c + 1200; ++i)
     {
         bf.writeChunk({.a = i});
     }
+    cout << "### WRITE END ###" << endl;
     cout << "count = " << bf.count() << endl;
+
+    for (int i = 0; i < bf.count(); ++i)
+    {
+        Item q;
+        bf.readChunk(q);
+        if (q.a != i + 1200)
+        {
+            cout << "TOO BAD!" << endl;
+        }
+    }
     return 0;
 }
